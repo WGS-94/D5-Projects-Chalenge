71316f5f94960822189c1421b6a33ad65636ffde
Wilson Dos Santos <wilson-senx@hotmail.com>
2022-07-26 20:59:29 -0300

    :bug: configure the routes

--- P5-Formulario-Multi-Etapas/src/routes.tsx
+++ P5-Formulario-Multi-Etapas/src/routes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { FormStep1 } from './pages/FormStep1';
 import { FormStep2 } from './pages/FormStep2';
@@ -7,9 +7,11 @@ import { FormStep3 } from './pages/FormStep3';
 export const Router = () => {
     return (
         <BrowserRouter>
-            <Route path="/" element={<FormStep1 />} />
-            <Route path="/step2" element={<FormStep2 />} />
-            <Route path="/step3" element={<FormStep3 />} />
+            <Routes>
+                <Route path="/" element={<FormStep1 />} />
+                <Route path="/step2" element={<FormStep2 />} />
+                <Route path="/step3" element={<FormStep3 />} />
+            </Routes>
         </BrowserRouter>
     );
 }
