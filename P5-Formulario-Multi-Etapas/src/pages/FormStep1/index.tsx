7b1b7bd669e12d11c54b86a423098ee360d96b3c
Wilson Dos Santos <wilson-senx@hotmail.com>
2022-07-25 23:00:20 -0300

    :fire: create pages and components

---
+++ P5-Formulario-Multi-Etapas/src/pages/FormStep1/index.tsx
@@ -0,0 +1,57 @@
+import { useHistory } from 'react-router-dom';
+import { useForm, FormActions } from '../../contexts/FormContext';
+import { Theme } from '../../components/Theme';
+import { ChangeEvent, useEffect } from 'react';
+
+import * as S from './styles';
+
+export const FormStep1 = () => {
+    const history = useHistory();
+    const { state, dispatch } = useForm();
+
+    useEffect(() => {
+        dispatch({
+            type: FormActions.setCurrentStep,
+            payload: 1
+        });
+    }, []);
+
+    const handleNextStep = () => {
+        if(state.name !== '') {
+            history.push('/step2');
+        } else {
+            alert("Preencha os dados.");
+        }
+    }
+
+    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: FormActions.setName,
+            payload: e.target.value
+        });
+    }
+
+    return (
+        <Theme>
+            <S.Container>
+                <p>Passo 1/3</p>
+                <h1>Vamos começar com seu nome</h1>
+                <p>Preencha o campo abaixo com seu nome completo.</p>
+
+                <hr/>
+
+                <label>
+                    Seu nome completo
+                    <input
+                        type="text"
+                        autoFocus
+                        value={state.name}
+                        onChange={handleNameChange}
+                    />
+                </label>
+
+                <button onClick={handleNextStep}>Próximo</button>
+            </S.Container>
+        </Theme>
+    );
+}
