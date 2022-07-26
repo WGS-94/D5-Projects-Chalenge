bbb234ebbfc1b898cb0b976891a1bd17afc76dea
Wilson Dos Santos <wilson-senx@hotmail.com>
2022-07-25 23:23:26 -0300

    :sparkles: Forms and svgs

--- P5-Formulario-Multi-Etapas/src/pages/FormStep3/index.tsx
+++ P5-Formulario-Multi-Etapas/src/pages/FormStep3/index.tsx
@@ -1,60 +1,76 @@
-import { useNavigate  } from 'react-router-dom';
+import { ChangeEvent, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import { useForm, FormActions } from '../../contexts/FormContext';
 import { Theme } from '../../components/Theme';
-import { ChangeEvent, useEffect } from 'react';
 
-import * as S from './styles';
+import * as C from './styles';
 
-export const FormStep1 = () => {
-
-    const navigate = useNavigate ();
-    
+export const FormStep3 = () => {
+    const navigate = useNavigate();
     const { state, dispatch } = useForm();
 
     useEffect(() => {
-        dispatch({
-            type: FormActions.setCurrentStep,
-            payload: 1
-        });
-    }, [dispatch]);
+        if(state.name === '') {
+            navigate('/');
+        } else {
+            dispatch({
+                type: FormActions.setCurrentStep,
+                payload: 3
+            });
+        }
+    }, [navigate, state.name, dispatch]);
 
     const handleNextStep = () => {
-        if(state.name !== '') {
-            //navigate.push('/step2');
-            navigate('/step2', { replace: true });
+        if(state.email !== '' && state.github !== '') {
+            console.log(state);
         } else {
-            alert("Preencha os dados.");
+            alert("Preencha os dados");
         }
     }
 
-    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
         dispatch({
-            type: FormActions.setName,
+            type: FormActions.setEmail,
+            payload: e.target.value
+        });
+    }
+    const handleGithubChange = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: FormActions.setGithub,
             payload: e.target.value
         });
     }
 
     return (
         <Theme>
-            <S.Container>
-                <p>Passo 1/3</p>
-                <h1>Vamos começar com seu nome</h1>
-                <p>Preencha o campo abaixo com seu nome completo.</p>
+            <C.Container>
+                <p>Passo 3/3</p>
+                <h1>Legal {state.name}, onde te achamos?</h1>
+                <p>Preencha com seus contatos para conseguirmos entrar em contato.</p>
 
                 <hr/>
 
                 <label>
-                    Seu nome completo
+                    Qual seu e-mail?
                     <input
-                        type="text"
-                        autoFocus
-                        value={state.name}
-                        onChange={handleNameChange}
+                        type="email"
+                        value={state.email}
+                        onChange={handleEmailChange}
                     />
                 </label>
 
-                <button onClick={handleNextStep}>Próximo</button>
-            </S.Container>
+                <label>
+                    Qual seu GitHub?
+                    <input
+                        type="url"
+                        value={state.github}
+                        onChange={handleGithubChange}
+                    />
+                </label>
+
+                <Link to="/step2" className="backButton">Voltar</Link>
+                <button onClick={handleNextStep}>Finalizar Cadastro</button>
+            </C.Container>
         </Theme>
     );
 }
