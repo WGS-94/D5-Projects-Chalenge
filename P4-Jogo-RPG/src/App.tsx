283f8c797c3b6f5013ce01c6b764c67229d1f147
Wilson Dos Santos <wilson-senx@hotmail.com>
2022-07-27 21:20:57 -0300

    :fire: developing rpg game from scratch

--- P4-Jogo-RPG/src/App.tsx
+++ P4-Jogo-RPG/src/App.tsx
@@ -1,13 +1,47 @@
-import * as C from './App.styles';
+import { useEffect } from 'react';
+
+import { Character } from './components/Character';
+import { useCharacter } from './hooks/useCharacter';
+
+import * as S from './App.styles';
 
 const App = () => {
+
+  const char = useCharacter('Wilson');
+  const char2 = useCharacter('Ganga');
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+  }, []);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch(e.code) {
+      case 'KeyA':
+      case 'ArrowLeft':
+        char.moveLeft();
+      break;
+      case 'KeyW':
+      case 'ArrowUp':
+        char.moveUp();
+      break;
+      case 'KeyD':
+      case 'ArrowRight':
+        char.moveRight();
+      break;
+      case 'KeyS':
+      case 'ArrowDown':
+        char.moveDown();
+      break;
+    }
+  }
+
   return (
-   
-    <C.Container>
-      <C.Header>
-      <C.HeaderText>Sistema Financeiro</C.HeaderText>
-      </C.Header>
-    </C.Container>
+    <S.Container>
+       <S.Map>
+        <Character x={char.x} y={char.y} side={char.side} name={char.name} />
+        <Character x={char2.x} y={char2.y} side={char2.side} name={char2.name} />
+      </S.Map>
+    </S.Container>
   );
 }
 
