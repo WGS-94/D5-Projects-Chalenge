99ace9ab7b9424cb6d5d9d9ecf588f2339c8d0e5
Wilson Dos Santos <wilson-senx@hotmail.com>
2022-08-07 20:46:50 -0300

    :fire: create globalstyle

---
+++ P3-Galeria-de-Fotos/src/global.ts
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from "styled-components";
+import "react-toastify/dist/ReactToastify.css";
+
+const GlobalStyle = createGlobalStyle`
+
+:root {
+  font-size: 60%;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: 'Montserrat', sans-serif;
+}
+
+ html, body, #root {
+  height: 100%;
+}
+
+/* body {
+  background: #2C2641;
+} */
+
+
+html, 
+input,
+button {
+  font-family: 700 1.6rem Roboto;
+} 
+
+@media (max-width: 768px) {
+
+html {
+  font-size: 87.5%;
+}
+
+}
+
+@media (min-width: 700px) {
+  :root {
+    font-size: 62.5%;
+  }
+}
+`;
+
+export default GlobalStyle;
